c17f7439856b9c1eb627f00825fc50da176be15f
Nathan S <33799153+nates36@users.noreply.github.com>
2025-08-30 19:46:25 -0600

    completed, I get so into problem solving that I forget to commit regularly, need to get better at that.

---
+++ script.js
@@ -0,0 +1,70 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let result = Math.random();
+    if (result <= 0.33) {
+        return "rock";
+    } else if (result > 0.33 && result <= 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }        
+}
+
+function getHumanChoice(string) {
+    let choice = prompt("Enter rock, paper, or scissors: ");
+    return choice;
+}
+
+// function playRound(humanChoice, computerChoice) {
+//     if (humanChoice.toUpperCase() === computerChoice.toUpperCase()) {
+//         console.log("It's a tie!");
+//     } else if (humanChoice.toUpperCase() === "ROCK" && computerChoice.toUpperCase() === "SCISSORS") {
+//         humanScore += 1;
+//         console.log("Human wins, 1 point!"); 
+//     } else if (humanChoice.toUpperCase() === "PAPER" && computerChoice.toUpperCase() === "ROCK") {
+//         humanScore += 1;
+//         console.log("Human wins, 1 point!");
+//     } else if (humanChoice.toUpperCase() === "SCISSORS" && computerChoice.toUpperCase() === "PAPER") {
+//         humanScore += 1;
+//         console.log("Human wins, 1 point!"); 
+//     } else {
+//         computerScore += 1;
+//         console.log("Computer wins, 1 point!");
+//     }
+// }
+
+function playGame(humanChoice, computerChoice) {
+    let rounds = 5;
+    for (let i = 0; i < rounds; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+
+        if (humanChoice.toUpperCase() === computerChoice.toUpperCase()) {
+            console.log("It's a tie! No points!");
+        } else if (humanChoice.toUpperCase() === "ROCK" && computerChoice.toUpperCase() === "SCISSORS") {
+            humanScore += 1;
+            console.log("Human wins, 1 point!");
+        } else if (humanChoice.toUpperCase() === "PAPER" && computerChoice.toUpperCase() === "ROCK") {
+            humanScore += 1;
+            console.log("Human wins, 1 point!");
+        } else if (humanChoice.toUpperCase() === "SCISSORS" && computerChoice.toUpperCase() === "PAPER") {
+            humanScore += 1;
+            console.log("Human wins, 1 point!");
+        } else {
+            computerScore += 1;
+            console.log("Computer wins, 1 point!");
+        }
+    }
+    console.log("Human: " + humanScore + ", Computer: " + computerScore)
+    if (humanScore === computerScore) {
+        console.log("It's a tie!");
+    } else if (humanScore > computerScore) {
+        console.log("HUMAN WINS!");
+    } else {
+        console.log("COMPUTER WINS!");
+    }
+}
+
+playGame();
